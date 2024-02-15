beba85690842b1f8f4d91dee57d39502a47613cd
Tulasiram <tulasiramkancharla119@gmail.com>
2024-02-15 14:00:35 +0530

    old

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
-const corsOptions = {
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use("/api/user/", userRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
