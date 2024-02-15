08a92cd7fbaa1824888fbdd4882ce1b468caa071
Tulasiram <tulasiramkancharla119@gmail.com>
2024-02-15 12:36:19 +0530

    cors bug solved

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 app.use("/api/user/", userRoutes);
 app.use("/api/chats", chatRoutes);
